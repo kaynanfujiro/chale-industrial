cbcf606c97ade8bd97d3c4f3aa45acd40b0ab2a5
Kaynan <kaynanfelipe28@hotmail.com>
2025-06-03 21:21:40 -0300

    Alter ip backend in frontend

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = "http://localhost:3000"; // URL base para o backend
+export const BASE_URL = "http://31.97.28.66:3000"; // URL base para o backend
 
 export const api = axios.create({
   baseURL: BASE_URL, // URL do backend
